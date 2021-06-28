8b76502e99e86f6ea3af449e321f93c1ce287baf
Mochamad Rafli Ramadhan <69032682+mochamadrafli2018@users.noreply.github.com>
2021-06-29 00:30:53 +0700

    Update handlerGET.js

--- src/handlerGET.js
+++ src/handlerGET.js
@@ -1,12 +1,15 @@
-const books = require('./books');
+let books = require('./books');
+ 
 // getAllBook
 const getAllBook = (request, h) => {
   // request dan h harus selalu ditulis
-  // query parameter reading === 1
+  // query parameter reading
   const {reading} = request.query;
+  // query parameter finished
   const {finished} = request.query;
   // const { name } = request.query;
-
+ 
+  // get all books with reading == true
   if (reading !== undefined) {
     if (reading == 1) {
       const bookAllRead = books.filter((i) => i.reading === true)
@@ -43,7 +46,8 @@ const getAllBook = (request, h) => {
     response.code(200);
     return response;
   }
-
+ 
+  // get all books with finished == true
   if (finished !== undefined) {
     if (finished == 1) {
       const bookAllFinished = books.filter((i) => i.finished === true)
@@ -80,38 +84,33 @@ const getAllBook = (request, h) => {
     response.code(200);
     return response;
   }
-  /*
-  if(nama !== undefined){
-    if(nama == 'Dicoding') {
-    }
-  }
-  */
-
+ 
+  // get all books
   else {
-    // getAllBook
-    const book = books.map((i) => {
-      return {
-        'id': i.id,
-        'name': i.name,
-        'publisher': i.publisher
-      }
-    });
-    if (book !== undefined) {
+    if (books !== undefined) {
       const response = h.response({
         status: 'success',
         data: {
-          book
-        }
+          // mengembalikan data id, name dan publisher
+          books : books.map((book) => {
+            return {
+              'id': book.id,
+              'name': book.name,
+              'publisher': book.publisher
+            }
+          })
+        } 
       });
       response.code(200);
       return response;
     }
   }
 }
-
+ 
 // getBookById
 const getBookById = (request, h) => {
-  // Access : http://localhost:5000/books/aWZBUW3JN_VBE-9I
+  // request params untuk mengambil data id yang dikirimkan melalui URL
+  // localhost:5000/books/{id}
   const {id} = request.params;
   const book = books.filter((n) => n.id === id)[0];
   if (book !== undefined) {
@@ -131,5 +130,5 @@ const getBookById = (request, h) => {
   response.code(404);
   return response;
 }
-
+ 
 module.exports = {getAllBook, getBookById};
